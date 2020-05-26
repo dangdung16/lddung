d5f23af291d393d3bb79ede9f04186d566f6963d
dangdung16 <64850390+dangdung16@users.noreply.github.com>
2020-05-26 22:43:16 +0700

    Add files via upload

---
+++ 6.2d.py
@@ -0,0 +1,10 @@
+lớp  hinhchunhat ( đối tượng ):
+    def  __init__ ( tự , a , b ):
+        tự . a = a
+        tự . b = b
+    def  dien_tich ( tự ):
+         tự trở về . a * tự . b
+a = int ( đầu vào ( "nhap chieu dai:" ))
+b = int ( đầu vào ( "nhap chieu rong:" ))
+c = hinhchunhat ( a , b )
+in ( "dien tich hinh chu nhat la:" , c . dien_tich ())
---
+++ 6.4d.py
@@ -0,0 +1,11 @@
+lớp  Cricle ( đối tượng ):
+    def  __init__ ( tự , r ):
+        tự . r = r
+    def  chu_vi ( tự ):
+        trả lại  2 * tự . r * 3,14
+    def  dien_tich ( tự ):
+         tự trở về . r ** 2 * 3.14
+r = int ( đầu vào ( "nhap ban kinh:" ))
+c = Cricle ( r )
+in ( "chu vi hinh tron ​​la:" , c . chu_vi ())
+in ( "dien tich hinh tron ​​la:" , c . dien_tich ())
